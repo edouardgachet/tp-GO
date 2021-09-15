eee88a907e91b4bcaa98c04406b9f74ae9b02efe
edouardgachet <edouard.gachet@hetic.net>
2021-09-15 16:05:51 +0200

    project

---
+++ go.mod
@@ -0,0 +1,14 @@
+module mesprojets/miniapi
+
+go 1.17
+
+require (
+	github.com/c-bata/go-prompt v0.2.6 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mattn/go-tty v0.0.3 // indirect
+	github.com/pkg/term v1.2.0-beta.2 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20210910150752-751e447fb3d0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func SimpleHandler(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		start := time.Now()
+
+		var hour int = start.Hour()
+		var minute int = start.Minute()
+
+		fmt.Fprintf(w, "il est : %dh%d", hour, minute)
+	}
+}
+
+func AddHandler(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		if err := req.ParseForm(); err != nil {
+			fmt.Println("Something went bad")
+			fmt.Fprintln(w, "Something went bad")
+			return
+		}
+		for key, value := range req.PostForm {
+			fmt.Println(key, "=>", value)
+		}
+		writeData(req.PostForm["author"][0], req.PostForm["entry"][0])
+		//fmt.Fprintf(w, "Information received: %v\n", req.PostForm["author"][0])
+	}
+}
+
+func EntriesHandler(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+
+		data, err := ioutil.ReadFile("test.txt") // lire le fichier
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Fprintf(w, string(data))
+	}
+}
+
+func writeData(author string, entry string) {
+
+	if author != "" && entry != "" {
+
+		file, err := os.OpenFile("test.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+		defer file.Close() // on ferme automatiquement à la fin de notre programme
+
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = file.WriteString(author + ":" + entry + "\n") // écrire dans le fichier
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/", SimpleHandler)
+	http.HandleFunc("/add", AddHandler)
+	http.HandleFunc("/entries", EntriesHandler)
+	//writeData()
+	http.ListenAndServe(":4567", nil)
+}
